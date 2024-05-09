b2b13c4ec5edbf6c96d1ca0b6b50995b0a275c58
charzynm <charzynm@wp.pl>
2024-05-09 12:29:22 +0000

    update

--- cypress/e2e/amazon_spec.cy.js
+++ cypress/e2e/amazon_spec.cy.js
@@ -10,12 +10,21 @@ describe('Amazon template spec', () => {
   })
 
   it('Search product', () => {
+    // Check if the search bar is visible
+    cy.get('#twotabsearchtextbox').should('be.visible');
+
     cy.get('#searchDropdownBox').select('Książki', {force:true})
 
     cy.get('#twotabsearchtextbox').type("Walter Moers", {force:true})
 
     cy.get('#nav-search-submit-button').click({force:true})
 
+    // Check if the search results contain books by Walter Moers
+    cy.contains('Walter Moers').should('exist');
+
     cy.get('[data-component-type="s-search-results"] a.a-link-normal.s-no-outline').first().click()
+
+    // Verify that the product detail page has loaded
+    cy.get('#productTitle').should('be.visible');
   })
 })
